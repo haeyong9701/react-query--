e048b4ddbff9fd2c99390e553e2846bef246c44c
haeyong9701 <haeyong9701@gmail.com>
2024-04-03 17:26:15 +0900

    feat: 로딩과 에러처리

--- src/Homepage.js
+++ src/Homepage.js
@@ -2,12 +2,21 @@ import { useQuery } from "@tanstack/react-query";
 import { getPosts } from "./api";
 
 function HomePage() {
-  const { data: postsData } = useQuery({
+  const {
+    data: postsData,
+    isPending,
+    isError,
+  } = useQuery({
     queryKey: ["posts"],
     queryFn: getPosts,
     staleTime: 60 * 1000,
     gcTime: 60 * 1000 * 10,
+    retry: 0,
   });
+
+  if (isPending) return "로딩 중입니다...";
+
+  if (isError) return "에러가 발생했습니다.";
 
   const posts = postsData?.results ?? [];
 
